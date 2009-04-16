4a078cc04873427076323480d80072450b8b815b
Hiroshi Sakai <ziguzagu@gmail.com>
2009-04-16 19:28:28 +0900

    Checking in changes prior to tagging of version 0.03.  Changelog diff is:

--- lib/HTML/Split.pm
+++ lib/HTML/Split.pm
@@ -3,7 +3,7 @@ package HTML::Split;
 use strict;
 use warnings;
 use 5.8.1;
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 use base qw( Class::Accessor::Fast );
 __PACKAGE__->mk_ro_accessors(qw( total_pages prev_page next_page ));
